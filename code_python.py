2b3e0db993a267ae430c278565d67106ead12f76
Erick Sá <erickdesa@CS-859.local>
2017-10-16 11:41:41 -0200

    my onboarding Git

---
+++ code_python.py
@@ -0,0 +1,5 @@
+name = raw_input("Whats is your name?") 
+age = raw_input("How old are you?")
+birthday = raw_input("When is your birthday?")
+
+print("Your name is %s, your age is %s and your birthday is %s" % (name, age, birthday))
