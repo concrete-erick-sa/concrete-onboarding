a63b17c4a54fac0b2454ee7ccfcd4713bf9c1fd3
Erick Sá <erickdesa@CS-859.local>
2017-10-16 12:03:42 -0200

    add mother and father names

--- code_python.py
+++ code_python.py
@@ -1,5 +1,9 @@
-name = raw_input("Whats is your name?") 
-age = raw_input("How old are you?")
-birthday = raw_input("When is your birthday?")
+name = raw_input("Whats is your name?\n") 
+age = raw_input("How old are you?\n")
+birthday = raw_input("When is your birthday?\n")
+mother = raw_input("What's your mother name?\n")
+father = raw_input("what's your father name?\n")
 
-print("Your name is %s, your age is %s and your birthday is %s" % (name, age, birthday))
+
+print("Your name is %s." "\nYour age is %s and your birthday is %s." 
+"\nYour parents are %s and %s." % (name, age, birthday, mother, father))
